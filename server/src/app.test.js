df0f66c17efbee25992685d11ad67ec0632d8679
Amy Xu <amy.he.xu@gmail.com>
2021-05-11 07:04:54 +1200

    chore: add test for the api

--- server/src/app.test.js
+++ server/src/app.test.js
@@ -42,3 +42,18 @@ describe("Test the over30 path", () => {
     expect(response.statusCode).toBe(200);
   });
 });
+
+describe("Test the api path", () => {
+  test("It should response the GET method with correct data", async () => {
+    const response = await request(app).get("/api");
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toStrictEqual({
+      people: [
+        { name: "Jim", age: 30, gender: "male", _id: "b3Fshn8F976TZCTg" },
+        { name: "Jane", age: 55, gender: "female", _id: "k3nEqkqlKmWZNejC" },
+        { name: "Bob", age: 20, gender: "male", _id: "oqnu2ZnPTebp04bG" },
+        { name: "Sally", age: 24, gender: "female", _id: "tKmv8RC6GlUnYcV3" },
+      ],
+    });
+  });
+});
