95004a76e2af43c4ade78f7629087c69808a0eed
Amy Xu <amy.he.xu@gmail.com>
2021-05-11 05:17:11 +1200

    chore: add lint

--- server/index.js
+++ server/index.js
@@ -1,90 +1,106 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-var Datastore = require('nedb'),
+var Datastore = require("nedb"),
   db = new Datastore({
-    filename: 'db.json',
-    autoload: true
+    filename: "db.json",
+    autoload: true,
   });
-var fs = require('fs');
-var handlebars = require('handlebars');
+var fs = require("fs");
+var handlebars = require("handlebars");
 
-app.get(['/everyone','/'], function (req, res) {
-  fs.readFile('everyone.hbs', 'utf8', (err, data) => {
+app.get(["/everyone", "/"], function (req, res) {
+  fs.readFile("everyone.hbs", "utf8", (err, data) => {
     var template = handlebars.compile(data);
     db.find({}, {}, (err, docs) => {
       var rendered = template({
-        people: docs
+        people: docs,
       });
-      res.contentType('text/html');
+      res.contentType("text/html");
       res.status(200).send(rendered);
     });
   });
 });
 
-app.get('/male', function (req, res) {
-  fs.readFile('everyone.hbs', 'utf8', (err, data) => {
+app.get("/male", function (req, res) {
+  fs.readFile("everyone.hbs", "utf8", (err, data) => {
     var template = handlebars.compile(data);
-    db.find({
-      gender: 'male'
-    }, {}, (err, docs) => {
-      var rendered = template({
-        people: docs
-      });
-      res.contentType('text/html');
-      res.status(200).send(rendered);
-    });
-  });
-});
-
-app.get('/female', function (req, res) {
-  fs.readFile('everyone.hbs', 'utf8', (err, data) => {
-    var template = handlebars.compile(data);
-    db.find({
-      gender: 'female'
-    }, {}, (err, docs) => {
-      var rendered = template({
-        people: docs
-      });
-      res.contentType('text/html');
-      res.status(200).send(rendered);
-    });
-  });
-});
-
-app.get('/under30', function (req, res) {
-  fs.readFile('everyone.hbs', 'utf8', (err, data) => {
-    var template = handlebars.compile(data);
-    db.find({
-      age: {
-        $lt: 30
+    db.find(
+      {
+        gender: "male",
+      },
+      {},
+      (err, docs) => {
+        var rendered = template({
+          people: docs,
+        });
+        res.contentType("text/html");
+        res.status(200).send(rendered);
       }
-    }, {}, (err, docs) => {
-      var rendered = template({
-        people: docs
-      });
-      res.contentType('text/html');
-      res.status(200).send(rendered);
-    });
+    );
   });
 });
 
-app.get('/over30', function (req, res) {
-  fs.readFile('everyone.hbs', 'utf8', (err, data) => {
+app.get("/female", function (req, res) {
+  fs.readFile("everyone.hbs", "utf8", (err, data) => {
     var template = handlebars.compile(data);
-    db.find({
-      age: {
-        $gte: 30
+    db.find(
+      {
+        gender: "female",
+      },
+      {},
+      (err, docs) => {
+        var rendered = template({
+          people: docs,
+        });
+        res.contentType("text/html");
+        res.status(200).send(rendered);
       }
-    }, {}, (err, docs) => {
-      var rendered = template({
-        people: docs
-      });
-      res.contentType('text/html');
-      res.status(200).send(rendered);
-    });
+    );
+  });
+});
+
+app.get("/under30", function (req, res) {
+  fs.readFile("everyone.hbs", "utf8", (err, data) => {
+    var template = handlebars.compile(data);
+    db.find(
+      {
+        age: {
+          $lt: 30,
+        },
+      },
+      {},
+      (err, docs) => {
+        var rendered = template({
+          people: docs,
+        });
+        res.contentType("text/html");
+        res.status(200).send(rendered);
+      }
+    );
+  });
+});
+
+app.get("/over30", function (req, res) {
+  fs.readFile("everyone.hbs", "utf8", (err, data) => {
+    var template = handlebars.compile(data);
+    db.find(
+      {
+        age: {
+          $gte: 30,
+        },
+      },
+      {},
+      (err, docs) => {
+        var rendered = template({
+          people: docs,
+        });
+        res.contentType("text/html");
+        res.status(200).send(rendered);
+      }
+    );
   });
 });
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log("Example app listening on port 3000!");
 });
