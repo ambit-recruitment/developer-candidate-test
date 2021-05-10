b6db0347589b312822d7169a24b7cbee21672d04
Amy Xu <amy.he.xu@gmail.com>
2021-05-11 07:23:59 +1200

    chore: filter capability as before

--- client/src/People.js
+++ client/src/People.js
@@ -1,8 +1,21 @@
 import React, { useState, useEffect } from "react";
 
+import FilterButton from "./FilterButton";
+
+const FILTER_MAP = {
+  EveryOne: () => true,
+  Male: (people) => people.gender === "male",
+  Female: (people) => people.gender === "female",
+  Over30: (people) => people.age >= 30,
+  Under30: (people) => people.age < 30,
+};
+
+const FILTER_NAMES = Object.keys(FILTER_MAP);
+
 function People() {
   const [peopleData, setPeopleData] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [filter, setFilter] = useState("EveryOne");
 
   useEffect(() => {
     const fetchData = async () => {
@@ -15,12 +28,42 @@ function People() {
     fetchData();
   }, []);
 
-  const display = peopleData.map((people) => (
-    <li key={people._id}>
-      {people.name}-{people.age}-{people.gender}
-    </li>
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
   ));
-  return <div>{loading ? "Loading" : <ul> {display} </ul>}</div>;
+  const peopleList = peopleData.filter(FILTER_MAP[filter]).map((people) => (
+    <tr>
+      <td>{people.name}</td>
+      <td>{people.gender}</td>
+      <td>{people.age}</td>
+    </tr>
+  ));
+
+  return (
+    <div>
+      <h2>People</h2>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <div>
+          {filterList}
+          <table>
+            <tr>
+              <th>Name</th>
+              <th>Gender</th>
+              <th>Age</th>
+            </tr>
+            {peopleList}
+          </table>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default People;
