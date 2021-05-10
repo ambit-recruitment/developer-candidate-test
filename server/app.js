db8d3588466b6b52a35044c7ad25324457975b94
Amy Xu <amy.he.xu@gmail.com>
2021-05-11 05:34:52 +1200

    chore: replace common logic with function to generate query param

--- server/app.js
+++ server/app.js
@@ -8,10 +8,48 @@ const db = new Datastore({
 const fs = require("fs");
 const handlebars = require("handlebars");
 
+const generateQueryParam = (path) => {
+  let queryParam;
+  switch (path) {
+    case "/male":
+      queryParam = {
+        gender: "male",
+      };
+      break;
+    case "/female":
+      queryParam = {
+        gender: "female",
+      };
+      break;
+    case "/under30":
+      queryParam = {
+        age: {
+          $lt: 30,
+        },
+      };
+      break;
+    case "/over30":
+      queryParam = {
+        age: {
+          $gte: 30,
+        },
+      };
+      break;
+    case "/everyone":
+    case "/":
+      queryParam = {};
+      break;
+    default:
+      throw new Error("Unsupported query");
+  }
+  return queryParam;
+};
+
 app.get(["/everyone", "/"], function (req, res) {
   fs.readFile("everyone.hbs", "utf8", (err, data) => {
     const template = handlebars.compile(data);
-    db.find({}, {}, (err, docs) => {
+    const queryParam = generateQueryParam(req.path);
+    db.find(queryParam, {}, (err, docs) => {
       const rendered = template({
         people: docs,
       });
@@ -24,80 +62,56 @@ app.get(["/everyone", "/"], function (req, res) {
 app.get("/male", function (req, res) {
   fs.readFile("everyone.hbs", "utf8", (err, data) => {
     const template = handlebars.compile(data);
-    db.find(
-      {
-        gender: "male",
-      },
-      {},
-      (err, docs) => {
-        const rendered = template({
-          people: docs,
-        });
-        res.contentType("text/html");
-        res.status(200).send(rendered);
-      }
-    );
+    const queryParam = generateQueryParam(req.path);
+    db.find(queryParam, {}, (err, docs) => {
+      const rendered = template({
+        people: docs,
+      });
+      res.contentType("text/html");
+      res.status(200).send(rendered);
+    });
   });
 });
 
 app.get("/female", function (req, res) {
   fs.readFile("everyone.hbs", "utf8", (err, data) => {
     const template = handlebars.compile(data);
-    db.find(
-      {
-        gender: "female",
-      },
-      {},
-      (err, docs) => {
-        const rendered = template({
-          people: docs,
-        });
-        res.contentType("text/html");
-        res.status(200).send(rendered);
-      }
-    );
+    const queryParam = generateQueryParam(req.path);
+    db.find(queryParam, {}, (err, docs) => {
+      const rendered = template({
+        people: docs,
+      });
+      res.contentType("text/html");
+      res.status(200).send(rendered);
+    });
   });
 });
 
 app.get("/under30", function (req, res) {
   fs.readFile("everyone.hbs", "utf8", (err, data) => {
     const template = handlebars.compile(data);
-    db.find(
-      {
-        age: {
-          $lt: 30,
-        },
-      },
-      {},
-      (err, docs) => {
-        const rendered = template({
-          people: docs,
-        });
-        res.contentType("text/html");
-        res.status(200).send(rendered);
-      }
-    );
+    const queryParam = generateQueryParam(req.path);
+    db.find(queryParam, {}, (err, docs) => {
+      const rendered = template({
+        people: docs,
+      });
+      res.contentType("text/html");
+      res.status(200).send(rendered);
+    });
   });
 });
 
 app.get("/over30", function (req, res) {
   fs.readFile("everyone.hbs", "utf8", (err, data) => {
     const template = handlebars.compile(data);
-    db.find(
-      {
-        age: {
-          $gte: 30,
-        },
-      },
-      {},
-      (err, docs) => {
-        const rendered = template({
-          people: docs,
-        });
-        res.contentType("text/html");
-        res.status(200).send(rendered);
-      }
-    );
+    const queryParam = generateQueryParam(req.path);
+    db.find(queryParam, {}, (err, docs) => {
+      const rendered = template({
+        people: docs,
+      });
+      res.contentType("text/html");
+      res.status(200).send(rendered);
+    });
   });
 });
 
